4255553a16a598ac8c62baa47912000de3c40d6f
Dima Uvin <u_win@mail.ru>
2020-12-15 14:34:59 +0700

    fix filter and validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,8 @@
 class Student < ApplicationRecord
   enum gender: [:female, :male]
   validates :name, :gender, :surname, presence: true
-  validates :cognomen, uniqueness: true, allow_nil: true
-  validates :cognomen, length: { in: 6..20 }, allow_nil: true
+  validates :cognomen, uniqueness: true, allow_nil: true, allow_blank: true
+  validates :cognomen, length: { in: 6..20 }, allow_nil: true, allow_blank: true
 
   scope :filter_by_selected_gender, -> (gender) { where gender: gender }
   scope :filter_by_selected_surname, -> (name) { where("surname like ?", "%#{name}%")}
